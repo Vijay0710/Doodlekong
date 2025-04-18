5b75c590a38944874c6b0ec06516b20a198194c1
Vijay A <vijaymangalani4588@gmail.com>
2025-04-18 21:27:23 +0530

    Added Drawer layout in DrawingActivity

--- app/src/main/java/com/plcoding/doodlekong/ui/drawing/DrawingActivity.kt
+++ app/src/main/java/com/plcoding/doodlekong/ui/drawing/DrawingActivity.kt
@@ -3,7 +3,10 @@ package com.plcoding.doodlekong.ui.drawing
 
 import android.graphics.Color
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
 import androidx.activity.viewModels
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Lifecycle
@@ -13,15 +16,19 @@ import com.plcoding.doodlekong.R
 import com.plcoding.doodlekong.databinding.ActivityDrawingBinding
 import com.plcoding.doodlekong.utils.Constants
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
-import androidx.core.graphics.toColorInt
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.RecyclerView
 
 @AndroidEntryPoint
 class DrawingActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDrawingBinding
-     private val viewModel: DrawingViewModel by viewModels()
+    private val viewModel: DrawingViewModel by viewModels()
+
+    private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var rvPlayers: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,11 +36,54 @@ class DrawingActivity : AppCompatActivity() {
         setContentView(binding.root)
         subscribeToUiStateUpdates()
 
+        toggle = ActionBarDrawerToggle(this, binding.root, R.string.open, R.string.close)
+
+        val header = layoutInflater.inflate(R.layout.nav_drawer_header, binding.navView)
+        rvPlayers = header.findViewById(R.id.rvPlayers)
+
+        handleDrawer()
+
         binding.colorGroup.setOnCheckedChangeListener { _, checkedId ->
             viewModel.checkRadioButton(
                 DrawingViewModel.ColorResourceId(checkedId)
             )
         }
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        toggle.syncState()
+    }
+
+    private fun handleDrawer() {
+
+        binding.root.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+
+        binding.ibPlayers.setOnClickListener {
+            binding.root.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            binding.root.openDrawer(GravityCompat.START)
+        }
+
+        binding.root.addDrawerListener(
+            object : DrawerLayout.DrawerListener {
+                override fun onDrawerSlide(drawerView: View, slideOffset: Float) {}
+
+                override fun onDrawerOpened(drawerView: View) {}
+
+                override fun onDrawerClosed(drawerView: View) {
+                    binding.root.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+                }
+
+                override fun onDrawerStateChanged(newState: Int) {}
+            }
+        )
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun setColor(color: Int) {
@@ -48,19 +98,20 @@ class DrawingActivity : AppCompatActivity() {
                 viewModel.selectedColorButtonId.collect {
                     binding.colorGroup.check(it.id)
 
-                    when(it.id) {
-                       R.id.rbRed -> setColor(Color.RED)
-                       R.id.rbGreen -> setColor(Color.GREEN)
-                       R.id.rbBlue -> setColor(Color.BLUE)
-                       R.id.rbYellow -> setColor(Color.YELLOW)
-                       R.id.rbOrange -> {
-                           setColor(ContextCompat.getColor(this@DrawingActivity, R.color.orange))
-                       }
-                       R.id.rbBlack -> setColor(Color.BLACK)
-                       R.id.rbEraser -> {
-                           binding.drawingView.setColor(Color.WHITE)
-                           binding.drawingView.setThickness(40f)
-                       }
+                    when (it.id) {
+                        R.id.rbRed -> setColor(Color.RED)
+                        R.id.rbGreen -> setColor(Color.GREEN)
+                        R.id.rbBlue -> setColor(Color.BLUE)
+                        R.id.rbYellow -> setColor(Color.YELLOW)
+                        R.id.rbOrange -> {
+                            setColor(ContextCompat.getColor(this@DrawingActivity, R.color.orange))
+                        }
+
+                        R.id.rbBlack -> setColor(Color.BLACK)
+                        R.id.rbEraser -> {
+                            binding.drawingView.setColor(Color.WHITE)
+                            binding.drawingView.setThickness(40f)
+                        }
                     }
                 }
             }
